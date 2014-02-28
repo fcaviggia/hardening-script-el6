d625db03bde987c22ece520111fa2208341809ba
Frank Caviggia <fcaviggia@users.noreply.github.com>
2014-02-27 20:05:48 -0500

    Removed gnome-screensaver PAM configuration
    Removed gnome-screensaver PAM configuration to put in an independent file. Changed forced-logout timeout to 120 minutes.

--- misc/gnome.sh
+++ misc/gnome.sh
@@ -114,7 +114,7 @@ EOF
 	gconftool-2 --direct \
               --config-source xml:readwrite:/etc/gconf/gconf.xml.mandatory \
               --type int \
-              --set /desktop/gnome/session/max_idle_time 25
+              --set /desktop/gnome/session/max_idle_time 120
 	gconftool-2 --direct \
               --config-source xml:readwrite:/etc/gconf/gconf.xml.mandatory \
               --type int \
@@ -138,36 +138,4 @@ EOF
               --type string \
               --set /apps/gnome-screensaver/mode blank-only
               
-	# Fix for gnome-screensaver
-	cat <<EOF > /etc/pam.d/gnome-screensaver
-#%PAM-1.0
-auth    [success=done ignore=ignore default=bad] pam_selinux_permit.so
-auth    required      pam_env.so
-auth    required      pam_lastlog.so inactive=35
-auth    required      pam_faillock.so preauth silent audit deny=3 even_deny_root root_unlock_time=900 unlock_time=604800 fail_interval=900
-auth    sufficient    pam_unix.so try_first_pass
-auth    [default=die] pam_faillock.so authfail audit deny=3 even_deny_root root_unlock_time=900 unlock_time=604800 fail_interval=900
-auth    sufficient    pam_faillock.so authsucc audit deny=3 even_deny_root root_unlock_time=900 unlock_time=604800 fail_interval=900
-auth    requisite     pam_succeed_if.so uid >= 500 quiet
-auth    required      pam_deny.so
-
-account required      pam_unix.so
-account required      pam_faillock.so
-account required      pam_lastlog.so inactive=35
-account sufficient    pam_localuser.so
-account sufficient    pam_succeed_if.so uid < 500 quiet
-account required      pam_permit.so
-
-#password required     pam_passwdqc.so min=disabled,disabled,16,12,8 random=42
-password required     pam_cracklib.so try_first_pass retry=3 minlen=14 dcredit=-1 ucredit=-1 ocredit=-1 lcredit=-1 difok=4
-password sufficient   pam_unix.so sha512 shadow try_first_pass use_authtok remember=24
-password required     pam_deny.so
-
-session required      pam_lastlog.so showfailed
-session optional      pam_keyinit.so revoke
-session required      pam_limits.so
-session [success=1 default=ignore] pam_succeed_if.so service in crond quiet use_uid
-session required      pam_unix.so
-EOF
-
 fi
