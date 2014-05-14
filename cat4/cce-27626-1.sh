22b520bfadb5b85bc0a0c2d45d943bfff9d5a66f
Shawn Wells <shawn@redhat.com>
2014-05-13 22:59:03 -0400

    Dropping CCE 27626 script

--- cat4/cce-27626-1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# CCE-27626-1: Install openswan package
-echo '==================================================='
-echo ' CCE-27626-1: Install openswan package
-echo '==================================================='
-yum -y install openswan
