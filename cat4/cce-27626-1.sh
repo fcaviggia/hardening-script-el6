809f373b42d3ccd24027f2e2c3f0c910133d3e8f
Shawn Wells <shawn@redhat.com>
2014-05-13 21:41:19 -0400

    New remediation, cce-27626-1.sh, install openswan

---
+++ cat4/cce-27626-1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# CCE-27626-1: Install openswan package
+echo '==================================================='
+echo ' CCE-27626-1: Install openswan package
+echo '==================================================='
+yum -y install openswan
