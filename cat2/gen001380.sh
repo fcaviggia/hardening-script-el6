3b1f65ce965adedad4d58007ab8cea5c3e6b6e5e
Shawn Wells <shawn@redhat.com>
2014-12-14 08:40:48 -0500

    removing file perm checks
    these are either covered in SSG directly (e.g. perms of /etc/passwd) or covered in RPM verify

--- cat2/gen001380.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-## (GEN001380: CAT II) (Previously – G048) The SA will ensure the /etc/passwd 
-## file has permissions of 644, or more restrictive.
-echo '==================================================='
-echo ' Patching GEN001380: Set permissions of /etc/passwd'
-echo '==================================================='
-chmod 644 /etc/passwd
--- cat2/gen001580.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-## (GEN001580: CAT II) (Previously – G058) The SA will ensure run control 
-## scripts have permissions of 755, or more restrictive.
-echo '==================================================='
-echo ' Patching GEN001580: Set permissions of run control'
-echo '                     scripts'
-echo '==================================================='
-chmod 755 /etc/rc.d/init.d/*
