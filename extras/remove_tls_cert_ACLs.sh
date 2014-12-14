ac64d97ce35934ac4e8344858f545e9f031c7b51
Shawn Wells <shawn@redhat.com>
2014-12-14 08:54:20 -0500

    moving gen008740 to extras/
    this is not an actual requirement, but is a fairly good idea
    
    moving to extras/ directory to reflect validity of the action, but stripped out
    text relating this back to an actual requirement

--- extras/remove_tls_cert_ACLs.sh
+++ extras/remove_tls_cert_ACLs.sh
@@ -11,17 +11,9 @@
 #|	    |   Creation	    |                    |            |
 #|__________|_______________________|____________________|____________|
 #######################DISA INFORMATION###############################
-#Group ID (Vulid): V-22574
-#Group Title: GEN008360
-#Rule ID: SV-26961r1_rule
-#Severity: CAT II
-#Rule Version (STIG-ID): GEN008360
 #Rule Title: If the system is using LDAP for authentication or account information, the LDAP TLS key file must not have an extended ACL.
 #
 #Vulnerability Discussion: LDAP can be used to provide user authentication and account information, which are vital to system security. The LDAP client configuration must be protected from unauthorized modification.
-#
-#Responsibility: System Administrator
-#IAControls: ECLP-1
 #
 #Check Content: 
 #Determine the key file.
@@ -34,12 +26,12 @@
 #######################DISA INFORMATION###############################
 
 #Global Variables#
-PDI=GEN008360
+PDI=remove_tls_cert_ACLs
 
 #Start-Lockdown
 if [ -e /etc/ldap.conf ]; then
 	echo '==================================================='
-	echo ' Patching GEN008280: Remove ACLs from LDAP TLS Key'
+	echo ' Remediating: Remove ACLs from LDAP TLS Key'
 	echo '==================================================='
 	TLSKEY=$(  grep -i '^tls_key' /etc/ldap.conf | awk '{print $2}' ) 
 	for line in $TLSKEY; do
