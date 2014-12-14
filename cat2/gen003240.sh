6fb9b14bf18ccf167894f0b40b5ce4caf0c20a6f
Shawn Wells <shawn@redhat.com>
2014-12-14 10:28:01 -0500

    removing gen003240, not a requirement, covere by rpm verify

--- cat2/gen003240.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-## (GEN003240: CAT II) (Previously – G622) The SA will ensure the owner and 
-## group owner of the cron.allow file is root.
-echo '==================================================='
-echo ' Patching GEN003240: Set owner and group owner of' 
-echo '                     the cron.allow file'
-echo '==================================================='
-chown root:root /etc/cron.allow
