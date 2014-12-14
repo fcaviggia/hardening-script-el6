69c5760dbde86b709db8b7f6a69df93519a7f2c4
Shawn Wells <shawn@redhat.com>
2014-12-14 10:08:16 -0500

    file perm checks not requirements or provided in SSG already

--- cat2/lnx00640.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-## (LNX00640: CAT II) The SA will ensure the owner of the /etc/securetty file 
-## is root.
-echo '==================================================='
-echo ' Patching LNX00640: Set securetty owner'
-echo '==================================================='
-chown root /etc/securetty
