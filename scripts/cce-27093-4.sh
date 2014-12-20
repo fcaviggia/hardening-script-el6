9b7988af51ebd01dcb629733ef69c6ce6aa31a81
Frank Caviggia <fcaviggia@users.noreply.github.com>
2014-12-19 21:21:41 -0500

    Update cce-27093-4.sh
    Added echo to announce script is running.

--- scripts/cce-27093-4.sh
+++ scripts/cce-27093-4.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+echo '==================================================='
+echo ' CCE-27093-4: Enable NTP Service'
+echo '==================================================='
+
 # CCE-27093-4: Enable NTP
 chkconfig ntpd on
