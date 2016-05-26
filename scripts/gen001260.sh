503139334bbee3899c9ad36f72d6b69f4bf955d3
jpschaaf <jpschaaf@jpschaaf.com>
2016-05-26 16:15:19 -0500

    Splitting this script in half, because running it as-is on a RedHat system causes a verification vailure when testing V-38447.  Only the first half of this script should run.

--- scripts/gen001260.sh
+++ scripts/gen001260.sh
@@ -2,9 +2,14 @@
 
 ## (GEN001260: CAT II) (Previously – G037) The SA will ensure all system log
 ## files have permissions of 640, or more restrictive.
+
+## Note:  This script has been split into two parts for systems
+##        where it's desired to change log file permissions without
+##        altering rc.sysinit. Part two isn't required per stig since
+##        /var/log/wtmp is not referenced from rsyslog.conf
+
 echo '==================================================='
 echo ' Patching GEN001260: Setting permissions of system'
 echo '                     log files.'
 echo '==================================================='
 find /var/log/ -type f -exec chmod 600 '{}' \;
-sed -i "s/chmod 0664/chmod 0600/" /etc/rc.d/rc.sysinit
